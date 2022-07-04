104fbc0d089af717681b7d3a33e4e5140883404a
Mariusz Bielesz <bielesz@yahoo.com>
2022-07-04 13:48:48 +0200

    Reworking the whole game

--- index.js
+++ index.js
@@ -1,62 +1,134 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const finalColumn = document.querySelector('[data-final-column]')
-const computerScoreSpan = document.querySelector('[data-computer-score]')
-const yourScoreSpan = document.querySelector('[data-your-score]')
-const SELECTIONS = [
-  {
-    name: 'rock',
-    emoji: '✊',
-    beats: 'scissors'
-  },
-  {
-    name: 'paper',
-    emoji: '✋',
-    beats: 'rock'
-  },
-  {
-    name: 'scissors',
-    emoji: '🤞',
-    beats: 'paper'
-  }
-]
+// app.js
+// https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/
+// Complete logic of game inside this function
 
-selectionButtons.forEach(selectionButton => {
-  selectionButton.addEventListener('click', e => {
-    const selectionName = selectionButton.dataset.selection
-    const selection = SELECTIONS.find(selection => selection.name === selectionName)
-    makeSelection(selection)
-  })
-})
+const game = () => {
+	let playerScore = 0;
+	let computerScore = 0;
+	let moves = 0;
 
-function makeSelection(selection) {
-  const computerSelection = randomSelection()
-  const yourWinner = isWinner(selection, computerSelection)
-  const computerWinner = isWinner(computerSelection, selection)
 
-  addSelectionResult(computerSelection, computerWinner)
-  addSelectionResult(selection, yourWinner)
+	// Function to
+	const playGame = () => {
+		const rockBtn = document.querySelector('.rock');
+		const paperBtn = document.querySelector('.paper');
+		const scissorBtn = document.querySelector('.scissor');
+		const playerOptions = [rockBtn,paperBtn,scissorBtn];
+		const computerOptions = ['rock','paper','scissors']
+		
+		// Function to start playing game
+		playerOptions.forEach(option => {
+			option.addEventListener('click',function(){
 
-  if (yourWinner) incrementScore(yourScoreSpan)
-  if (computerWinner) incrementScore(computerScoreSpan)
+				const movesLeft = document.querySelector('.movesleft');
+				moves++;
+				movesLeft.innerText = `Moves Left: ${10-moves}`;
+				
+
+				const choiceNumber = Math.floor(Math.random()*3);
+				const computerChoice = computerOptions[choiceNumber];
+
+				// Function to check who wins
+				winner(this.innerText,computerChoice)
+				
+				// Calling gameOver function after 10 moves
+				if(moves == 10){
+					gameOver(playerOptions,movesLeft);
+				}
+			})
+		})
+		
+	}
+
+	// Function to decide winner
+	const winner = (player,computer) => {
+		const result = document.querySelector('.result');
+		const playerScoreBoard = document.querySelector('.p-count');
+		const computerScoreBoard = document.querySelector('.c-count');
+		player = player.toLowerCase();
+		computer = computer.toLowerCase();
+		if(player === computer){
+			result.textContent = 'Tie'
+		}
+		else if(player == 'rock'){
+			if(computer == 'paper'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+
+			}else{
+				result.textContent = 'Player Won'
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+		else if(player == 'scissors'){
+			if(computer == 'rock'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+			}else{
+				result.textContent = 'Player Won';
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+		else if(player == 'paper'){
+			if(computer == 'scissors'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+			}else{
+				result.textContent = 'Player Won';
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+	}
+
+	// Function to run when game is over
+	const gameOver = (playerOptions,movesLeft) => {
+
+		const chooseMove = document.querySelector('.move');
+		const result = document.querySelector('.result');
+		const reloadBtn = document.querySelector('.reload');
+
+		playerOptions.forEach(option => {
+			option.style.display = 'none';
+		})
+
+	
+		chooseMove.innerText = 'Game Over!!'
+		movesLeft.style.display = 'none';
+
+		if(playerScore > computerScore){
+			result.style.fontSize = '2rem';
+			result.innerText = 'You Won The Game'
+			result.style.color = '#308D46';
+		}
+		else if(playerScore < computerScore){
+			result.style.fontSize = '2rem';
+			result.innerText = 'You Lost The Game';
+			result.style.color = 'red';
+		}
+		else{
+			result.style.fontSize = '2rem';
+			result.innerText = 'Tie';
+			result.style.color = 'grey'
+		}
+		reloadBtn.innerText = 'Restart';
+		reloadBtn.style.display = 'flex'
+		reloadBtn.addEventListener('click',() => {
+			window.location.reload();
+		})
+	}
+
+
+	// Calling playGame function inside game
+	playGame();
+	
 }
 
-function incrementScore(scoreSpan) {
-  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
+// Calling the game function
+game();
 
-function addSelectionResult(selection, winner) {
-  const div = document.createElement('div')
-  div.innerText = selection.emoji
-  div.classList.add('result-selection')
-  if (winner) div.classList.add('winner')
-  finalColumn.after(div)
-}
-
-function isWinner(selection, opponentSelection) {
-  return selection.beats === opponentSelection.name
-}
-
-function randomSelection() {
-  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-  return SELECTIONS[randomIndex]
-}
